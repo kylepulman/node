c3cd8e518761d59ae385248f6d6a63945a65df7e
Kyle P. Ulman <kylepulman@proton.me>
2024-12-03 23:25:23 -0500

    feat: add auth endpoint

--- src/track/index.ts
+++ src/track/index.ts
@@ -8,12 +8,14 @@ const program = new Command()
 program
   .name('track')
   .action(() => {
-    startServerInBackground([`${import.meta.dirname}/server.js`])
+    console.log('Hello, Track!') /* eslint-disable-line no-console */
   })
 
 program
   .command('login')
   .action(async () => {
+    startServerInBackground([`${import.meta.dirname}/server.js`])
+
     const { href } = buildUrl(env('SPOTIFY_AUTHORIZATION_URL'), {
       client_id: env('SPOTIFY_CLIENT_ID'),
       redirect_uri: env('SPOTIFY_REDIRECT_URI'),
--- src/track/server.ts
+++ src/track/server.ts
@@ -1,5 +1,60 @@
+import { RequestError, env } from '../lib/index.js'
 import express from 'express'
 
 const app = express()
+
+app.get('/api/auth', async (req, res) => {
+  const { code, error } = req.query
+
+  if (error || typeof code !== 'string') {
+    res
+      .status(400)
+      .json(new RequestError(
+        400,
+        error,
+        'Error requesting authorization code.',
+      ))
+
+    return
+  }
+
+  const response = await fetch(env('SPOTIFY_TOKEN_URL'), {
+    body: new URLSearchParams({
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: env('SPOTIFY_REDIRECT_URI'),
+    }).toString(),
+    headers: {
+      'Authorization': `Basic ${Buffer.from(`${env('SPOTIFY_CLIENT_ID')}:${env('SPOTIFY_CLIENT_SECRET')}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    method: 'POST',
+  })
+
+  let body
+
+  if (response.headers.get('Content-Type')?.includes('application/json')) {
+    body = await response.json()
+  }
+  else {
+    body = await response.text()
+  }
+
+  if (response.ok) {
+    res.json(body)
+
+    process.exit()
+  }
+
+  res
+    .status(response.status)
+    .json(new RequestError(
+      response.status,
+      body,
+      'Error requesting token.',
+    ))
+
+  process.exit()
+})
 
 app.listen(3000)
